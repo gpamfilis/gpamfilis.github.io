5d8a0b2c38d98cec44d0507c2825f9e07a65fd28
George Pamfilis <gpamfilis@gmail.com>
2017-01-02 05:59:48 +0200

    created the e-orders docs?

--- doc/conf.py
+++ doc/conf.py
@@ -37,8 +37,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode', #
-    'sphinx.ext.githubpages',
-              "sphinxtogithub"]
+    'sphinx.ext.githubpages']
 
 
 
@@ -93,15 +92,15 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'alabaster'
+html_theme = 'alabaster'
 
 
 #extra
-import sphinx_rtd_theme
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
-
+# import sphinx_rtd_theme
+# html_theme = 'sphinx_rtd_theme'
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+#
+#
 
 
 

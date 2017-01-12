b3cace9740a5993b0339ac309d5a2f188b4b15b9
George Pamfilis <gpamfilis@gmail.com>
2017-01-12 22:10:56 +0200

    created the e-orders docs?

--- copy_and_push.sh
+++ copy_and_push.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
  sudo cp -avr /home/kasper/PycharmProjects/e_orders/doc/_build/html/. /home/kasper/PycharmProjects/gpamfilis.github.io/
 # sudo cp -avr ./e_orders/doc/ ./gpamfilis.github.io/
-#mv ./_static/ ./static/
+mv ./_static/ ./static/
 
-git add *
-git commit -m "new"
-git push origin master
+#git add *
+#git commit -m "new"
+#git push origin master
